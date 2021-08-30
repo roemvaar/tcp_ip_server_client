154025ce2c55183e3106a5ad6d9deb96e25467cd
Roberto Valenzuela <valenzuelarober@gmail.com>
2021-08-29 21:59:01 -0500

    Update tcp_client.cpp

--- src/tcp_client.cpp
+++ src/tcp_client.cpp
@@ -1,3 +1,17 @@
 #include "../include/tcp_client.h"
 
 using namespace std;
+
+class TCPClient {
+    public:
+        TCPClient();
+        ~TCPClient();
+    private:    
+};
+
+
+int main(int argc, char *argv[])
+{
+    cout << "TCP Client" << endl;
+}
+  
